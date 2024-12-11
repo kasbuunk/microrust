2a6a7d49507becc671353e30af660b214cdbee73
Kas Buunk <kasbuunk@icloud.com>
2024-12-11 16:02:59 +0100

    feat: calculate num of neighbours

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,11 @@ fn next(grid: Grid) -> Grid {
     for (index_row, row) in grid.iter().enumerate() {
         for (index_column, cell) in row.iter().enumerate() {
             let num_neighbours = neighbours(grid, index_row, index_column);
-            if num_neighbours == 2 || num_neighbours == 3 {
+            if num_neighbours == 3 {
+                updated[index_row][index_column] = 1;
+                continue;
+            }
+            if *cell == 1 && (num_neighbours == 2 || num_neighbours == 3) {
                 updated[index_row][index_column] = 1;
             }
         }
@@ -45,7 +49,27 @@ fn next(grid: Grid) -> Grid {
 }
 
 fn neighbours(grid: Grid, row: usize, column: usize) -> u8 {
-    return (row + column) as u8;
+    let row_count = grid.len();
+    let col_count = grid[0].len();
+
+    let row_above = (row + row_count - 1) % row_count;
+    let row_below = (row + 1) % row_count;
+
+    let column_left = (column + col_count - 1) % col_count;
+    let column_right = (column + 1) % col_count;
+
+    let cells = [
+        grid[row_above][column_left],
+        grid[row_above][column],
+        grid[row_above][column_right],
+        grid[row][column_left],
+        grid[row][column_right],
+        grid[row_below][column_left],
+        grid[row_below][column],
+        grid[row_below][column_right],
+    ];
+
+    cells.iter().sum()
 }
 
 fn block() -> Grid {
