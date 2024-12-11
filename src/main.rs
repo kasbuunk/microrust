0b80211237b6043b7dd71e761784431b23674e17
Kas Buunk <kasbuunk@icloud.com>
2024-12-11 14:50:23 +0100

    feat: show blinking heart

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,33 @@
 #![no_main]
 
 use cortex_m_rt::entry;
-use microbit as _;
+use embedded_hal::delay::DelayNs;
+use microbit::board::Board;
+use microbit::display::blocking::Display;
+use microbit::hal;
 use panic_halt as _;
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
-    loop {}
+    loop {
+        // take the board
+        let board = Board::take().unwrap();
+        // make a timer
+        let mut timer = hal::Timer::new(board.TIMER0);
+        // create the Display
+        let mut display = Display::new(board.display_pins);
+        // and light up some LEDs
+        let heart = [
+            [0, 1, 0, 1, 0],
+            [1, 0, 1, 0, 1],
+            [1, 0, 0, 0, 1],
+            [0, 1, 0, 1, 0],
+            [0, 0, 1, 0, 0],
+        ];
+        loop {
+            display.show(&mut timer, heart, 1000);
+            display.clear();
+            timer.delay_ms(250);
+        }
+    }
 }
